3c8c7b604c7feb388f7f06ca45b0c04038d25d44
Benoumessad-Mohamed-El-Amine <benoumessadmohamedelamine@gmail.com>
2026-02-16 01:43:47 +0100

    fix static files for production on render

--- password_checker/settings.py
+++ password_checker/settings.py
@@ -142,6 +142,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# WhiteNoise compression
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
