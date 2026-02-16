e310267efb41cf47eb4196813056ea644b44cba4
Benoumessad-Mohamed-El-Amine <benoumessadmohamedelamine@gmail.com>
2026-02-16 13:03:15 +0100

    test

--- password_checker/settings.py
+++ password_checker/settings.py
@@ -128,3 +128,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+WHITENOISE_INDEX_FILE = True
