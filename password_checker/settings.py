fa2d510d4b133c3d1ae34615cc834ee3717640d7
Benoumessad-Mohamed-El-Amine <benoumessadmohamedelamine@gmail.com>
2026-02-16 12:48:19 +0100

    Fix static files with WhiteNoise

--- password_checker/settings.py
+++ password_checker/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'checker.middleware.SecurityHeadersMiddleware',  # Custom security headers
+    'checker.middleware.SecurityHeadersMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Custom security headers
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,6 +141,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
